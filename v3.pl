00bbb08a48ebba6a2db5332965d05d23014149a3
childgo <65421844+childgo@users.noreply.github.com>
2020-05-15 16:50:44 -0400

    Create v3.pl

---
+++ v3.pl
@@ -0,0 +1,89 @@
+
+#!/usr/bin/perl
+
+use warnings;
+use Sys::Hostname;
+use Getopt::Long;
+use Term::ANSIColor qw(:constants);
+use POSIX;
+use File::Find;
+use Term::ANSIColor;
+
+$ENV{'PATH'} = '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin';
+
+## OPTIONS ##
+
+my %opts;
+my $domain;
+my $sent;
+my $blacklists;
+my $help;
+
+GetOptions(
+    \%opts,
+    'domain=s'     => \$domain,
+    'sent:s'       => \$sent,
+    'email:s'      => \$email,
+    'blacklists:s' => \$blacklists,
+    'help'         => \$help
+) or die("Please see --help\n");
+
+## GLOBALS ##
+
+
+
+
+
+if ($domain)
+{
+domain_exist();
+}
+
+else
+{    
+print_normal("Please choose something.\n");
+}
+
+## Colors ##
+
+sub print_info {
+    my $text = shift;
+    print BOLD YELLOW ON_BLACK $text;
+    print color 'reset';
+}
+
+sub print_warning {
+    my $text = shift;
+    print BOLD RED ON_BLACK "$text";
+    print color 'reset';
+}
+
+sub print_normal {
+    my $text = shift;
+    print BOLD CYAN ON_BLACK "$text";
+    print color 'reset';
+}
+
+
+
+
+
+### DOMAIN CHEX ###
+sub domain_exist {
+    open( USERDOMAINS, "/etc/userdomains" );
+    while (<USERDOMAINS>) {
+        if (/^$domain: (\S+)/i) {
+            my $user = $1;
+            print_info("\n[INFO] *");
+            print_normal(" The domain $domain is owned by $user.\n");
+            my $suspchk = "/var/cpanel/suspended/$user";
+            if ( -e $suspchk ) {
+                print_warning("[WARN] * The user $user is SUSPENDED.\n");
+            }
+            return;
+        }
+    }
+    print_warning(
+        "[WARN] * The domain $domain DOES NOT exist on this server.\n");
+    close(USERDOMAINS);
+}

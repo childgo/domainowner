eb32a1f64d5ee4a66b6b897f4166a7a23d04abd9
childgo <65421844+childgo@users.noreply.github.com>
2020-05-15 16:40:30 -0400

    Update v2.pl

--- v2.pl
+++ v2.pl
@@ -29,62 +29,21 @@ GetOptions(
 
 ## GLOBALS ##
 
-my $hostname = hostname;
-chomp( my $queue_cnt = `exim -bpc` );
-my @local_ipaddrs_list = get_local_ipaddrs();
-get_local_ipaddrs();
+
 
 ## GUTS ##
 
 if ($domain) {    ## --domain{
-    hostname_check();
+
     domain_exist();
-    domain_filters();
-    check_local_or_remote();
-    mx_check();
-    mx_consistency();
-    domain_resolv();
-    check_spf();
-    check_dkim();
-}
 
-elsif ($help) {    ##--help
-    help();
-}
-
-elsif ( defined $sent ) {
-    sent_email();
-}
-
-elsif ( defined $email ) {
-    if ( $email =~
-/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-      )
-    {
-        does_email_exist();
-        email_valiases();
-        email_filters();
-        email_quota();
-    }
-    else {
-        die "Please enter a valid email address\n";
-    }
-}
-elsif ( defined $blacklists ) {
-    check_blacklists();
 }
 
 else {    ## No options passed.
-    is_exim_running();
-    print_info("\n[INFO] * ");
+
     print_normal(
         "There are currently $queue_cnt messages in the Exim queue.\n");
-    nobodyspam_tweak();
-    check_for_phphandler();
-    port_26();
-    custom_etc_mail();
-    rdns_lookup();
-    check_closed_ports();
+
 }
 
 ## Colors ##
